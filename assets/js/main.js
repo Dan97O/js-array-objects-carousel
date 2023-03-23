98d0a5f0e35163aefca5999d0ef6bb1a71c8c874
Dan97O <dan97-oddo@live.it>
2023-03-23 16:49:34 +0100

    add milestone1 and milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,49 +53,107 @@ const images = [
 //Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
 
 const imagesElement = document.querySelector('.slider > .images');
-let activeImage = 0
-
-const photo = images.forEach((img, i) => {
-console.log(`${img.image}`);
-const imgElement = `<img class="img-fluid ${i === activeImage ? 'active' : ''}"  src="./assets/${img.image}" alt="">`
-//console.log(imgElement);
-imagesElement.insertAdjacentHTML("beforeend", imgElement)
-})
-
-
-const nextEl = document.querySelector('.next')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let activeImage = 0;
+let activeText = 0;
 
 
 //Milestone 1:
 //Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
 //Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+const photo = images.forEach((img, i) => {
+//console.log(`${img.image}`);
+const imgElement = `
+<img class="img-fluid ${i === activeImage ? 'active' : ''}"  src="./assets/${img.image}" alt="">
+<div class="card-body ${i === activeImage ? 'active' : ''}">
+  <h3>
+    ${img.title} 
+  </h3>
+  <p>
+    ${img.text}
+  </p>
+</div>`
+imagesElement.insertAdjacentHTML("beforeend", imgElement)
+//console.log(img.title);
+})
 
 
-//Milestone 2:
-//Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
+
+const slideImagesElements = document.querySelectorAll('.slider > .images > img')
+const slideTitleInfo = document.querySelector('.card-body')
+//console.log('qui3',slideTitleInfo);
+
+const nextEl = document.querySelector('.next')
 
 
+// listen for clicks on next button
+nextEl.addEventListener('click', function () {
+  console.log('cliccato next');
 
+
+  // select the current slide
+  const currentSlide = slideImagesElements[activeImage];
+
+ 
+  //console.log(currentSlide, 'qui');
+
+  // remove the active class from the active image
+  currentSlide.classList.remove('active');
+  
+  // incremente the value of the activeImage variable
+  activeImage++;
+  
+  // increment the value of activeImage of 1 every time we click on the next button
+
+  //Milestone 2:
+  //Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
+  if (activeImage == images.length) {
+      activeImage = 0;
+  }
+
+
+  // select the next slide
+  console.log(activeImage);
+  const nextImage = slideImagesElements[activeImage];
+
+
+  
+  // add the active class
+  console.log(nextImage);
+  nextImage.classList.add('active');
+ 
+
+ 
+})
+
+
+// listen for clicks on prev button
+const prevEl = document.querySelector('.prev')
+prevEl.addEventListener('click', function () {
+    //console.log('cliccato prev');
+
+    //console.log(slideImagesElements); //array[index]
+    // select the current slide
+    const currentSlide = slideImagesElements[activeImage]
+    //console.log(currentSlide);
+    // remove the active class from the active image
+    currentSlide.classList.remove('active')
+    // incremente the value of the activeImage variable
+    activeImage-- // increment the value of activeImage of 1 every time we click on the next button
+
+    //Milestone 2:
+    //Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
+    if (activeImage == -1) {
+        activeImage = images.length - 1;
+    }
+
+    // select the next slide
+    //console.log(activeImage);
+    const nextImage = slideImagesElements[activeImage]
+    // add the active class
+    //console.log(nextImage);
+    nextImage.classList.add('active')
+
+})
 
 
 
